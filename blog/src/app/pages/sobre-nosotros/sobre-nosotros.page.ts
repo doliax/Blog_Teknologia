4207cc1247485c432e8f8b86bac6e027fefa060a
doliax <ramon.moncholi08@gmail.com>
2023-09-15 17:30:23 +0200

    texto post correcto y mejora logica ion-chip

--- blog/src/app/pages/sobre-nosotros/sobre-nosotros.page.ts
+++ blog/src/app/pages/sobre-nosotros/sobre-nosotros.page.ts
@@ -8,34 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class SobreNosotrosPage implements OnInit {
   constructor() { }
 
-
+   languages: string[] = ['Python', 'JavaScript', 'TypeScript'];
+   robotica: string[] = ['PLC', 'Maquinaria Industrial'];
 
   ngOnInit() {
 
-    
-  //Dos ids asignados
+    this.crearLen();
+    this.crearRobot();
+  
+
+  }
+
+  crearLen(){
+    //Dos ids asignados
   const chip_prog = document.getElementById('prog');
-  const chip_rob = document.getElementById('rob');
+ 
 
   const div_languages = document.getElementById('languagesList');
-  const robotica_languages = document.getElementById('roboticaList');
+  
 
- 
- if (chip_prog && div_languages) {
+  if (chip_prog && div_languages) {
+  
   
    chip_prog.addEventListener('click', () => {
-     //Lista lenguajes
-     const languages = ['Python', 'JavaScript', 'TypeScript'];
-
+   
      //Clear
      div_languages.innerHTML = '';
 
      //Creo la lista
      const ol = document.createElement('ol');
-    ol.setAttribute('style', 'list-style-type:none');
+     ol.setAttribute('style', 'list-style-type:none');
 
      //Recorro la lista con un forEach, en cada elemento los añado como texto al li que he creado y este lo añado al ol
-     languages.forEach(language => {
+     this.languages.forEach(language => {
        const li = document.createElement('li');
        li.textContent = language;
        ol.appendChild(li);
@@ -44,45 +49,40 @@ export class SobreNosotrosPage implements OnInit {
      //Una vez tenga todos en el ol lo añado al div que tengo
      div_languages.appendChild(ol);
 
+
    });
  };
+  };
 
- 
-
- if(chip_prog){
-  chip_prog.addEventListener('click', () => {
-    console.log("dato");
+  crearRobot(){
+    const chip_rob = document.getElementById('rob');
+    const robotica_languages = document.getElementById('roboticaList');
+    if (chip_rob && robotica_languages) {
+      chip_rob.addEventListener('click', () => {
+      
+        //Clear
+        robotica_languages.innerHTML = '';
     
-})
- }
-
- if (chip_rob && robotica_languages) {
-  chip_rob.addEventListener('click', () => {
-    //Lista de robotica
-    const robotica = ['PLC', 'Maquinaria Industrial'];
-
-    //Clear
-    robotica_languages.innerHTML = '';
-
-    //Creo la lista
-    const ol = document.createElement('ol');
-   ol.setAttribute('style', 'list-style-type:none');
-
-    //Recorro la lista con un forEach, en cada elemento los añado como texto al li que he creado y este lo añado al ol
-    robotica.forEach(ro => {
-      const li = document.createElement('li');
-      li.textContent = ro;
-      ol.appendChild(li);
-    });
-
-    //Una vez tenga todos en el ol lo añado al div que tengo
-    robotica_languages.appendChild(ol);
-
+        //Creo la lista
+        const ol = document.createElement('ol');
+       ol.setAttribute('style', 'list-style-type:none');
     
+        //Recorro la lista con un forEach, en cada elemento los añado como texto al li que he creado y este lo añado al ol
+        this.robotica.forEach(ro => {
+          const li = document.createElement('li');
+          li.textContent = ro;
+          ol.appendChild(li);
+        });
     
-  });
-}
+        //Una vez tenga todos en el ol lo añado al div que tengo
+        robotica_languages.appendChild(ol);
+    
+        
+        
+      });
+    }
+  }
 
 }
 
-}
+
